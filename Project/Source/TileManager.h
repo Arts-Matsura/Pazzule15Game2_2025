d5be8fbbc3c8a439145100f2806e54248058aa4b
Arts-Matsura <k235025@kccollege.ac.jp>
2025-12-15 11:44:14 +0900

    tileSwap

--- Project/Source/TileManager.h
+++ Project/Source/TileManager.h
@@ -2,6 +2,8 @@
 #include "../Library/gameObject.h"
 #include "Tile.h"
 #include <vector>
+#include "ActionEvent.h"
+
 
 class TileManager : public GameObject
 {
@@ -17,9 +19,16 @@ public:
 
 	void OnInputTile(Vector2I dir);
 
+	bool IsPerfectCheck();
 
+	ActionEvent<> OnMoveTile;
+	ActionEvent<> OnPerfectTile;
 
 private:
+	void OnMoveEvent();
+	void OnPerfectEvent();
+	void MoveRandomTile();
+
 	std::vector<std::vector<Tile*>> tile_list;
 	Tile* air_tile;
 };
